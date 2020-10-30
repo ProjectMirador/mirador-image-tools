9811fcebdc97591840f88cc51fc10accfe96a374
Matthias Lindinger <morpheus-87@users.noreply.github.com>
2020-10-30 08:47:19 +0100

    Add german translations

--- src/translations.js
+++ src/translations.js
@@ -1,4 +1,17 @@
 const translations = {
+  de: {
+    brightness: 'Helligkeit',
+    collapse_open: 'Bildbearbeitung ausblenden',
+    collapse_close: 'Bildbearbeitung einblenden',
+    contrast: 'Kontrast',
+    flip: 'Spiegeln',
+    greyscale: 'Darstellung in Graustufen',
+    invert: 'Umkehrung der Farben',
+    revert: 'Einstellungen zurücksetzen',
+    rotateLeft: 'Rotation links',
+    rotateRight: 'Rotation rechts',
+    saturation: 'Sättigung',
+  },
   en: {
     brightness: 'Brightness',
     collapse_open: 'Collapse image tools',
