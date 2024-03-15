ccebfb46d46fd194b495d2bcd4b92915d99db276
Chris Beer <chris@cbeer.info>
2024-03-15 11:15:35 -0700

    Move code from miradorImageToolsPlugin.js to the src/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-export miradorImageToolsPlugin from './plugins/miradorImageToolsPlugin';
+import withTheme from '@material-ui/core/styles/withTheme';
+import * as actions from 'mirador/dist/es/src/state/actions';
+import { getWindowConfig, getViewer, getContainerId } from 'mirador/dist/es/src/state/selectors';
+import MiradorImageTools from './plugins/MiradorImageTools';
+import MiradorImageToolsMenuItem from './plugins/MiradorImageToolsMenuItem';
+import translations from './translations';
+
+export const miradorImageToolsPlugin = [
+  {
+    target: 'OpenSeadragonViewer',
+    mapDispatchToProps: {
+      updateWindow: actions.updateWindow,
+      updateViewport: actions.updateViewport,
+    },
+    mapStateToProps: (state, { windowId }) => ({
+      containerId: getContainerId(state),
+      enabled: getWindowConfig(state, { windowId }).imageToolsEnabled || false,
+      open: getWindowConfig(state, { windowId }).imageToolsOpen || false,
+      viewConfig: getViewer(state, { windowId }) || {},
+    }),
+    mode: 'add',
+    component: withTheme(MiradorImageTools),
+    config: {
+      translations,
+    },
+  },
+  {
+    target: 'WindowTopBarPluginMenu',
+    component: MiradorImageToolsMenuItem,
+    mode: 'add',
+    mapDispatchToProps: {
+      updateWindow: actions.updateWindow,
+    },
+    mapStateToProps: (state, { windowId }) => ({
+      enabled: getWindowConfig(state, { windowId }).imageToolsEnabled || false,
+    }),
+  },
+];
