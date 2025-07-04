57e8b3fc0e0e42fcfa52be904c8ae8b28afb893e
Lutz Helm <helm@ub.uni-leipzig.de>
2025-07-04 18:02:17 +0200

    Update rollup external options in vite config

--- vite.config.js
+++ vite.config.js
@@ -40,7 +40,13 @@ export default defineConfig({
           name: 'MiradorDlPlugin',
         },
         rollupOptions: {
-          external: [...Object.keys(pkg.peerDependencies || {}), '__tests__/*', '__mocks__/*'],
+          external: (id) => {
+            const peers = Object.keys(pkg.peerDependencies);
+            return peers.indexOf(id) > -1
+              || peers.find((peer) => id.startsWith(`${peer}/`))
+              || id.startsWith('__tests__/')
+              || id.startsWith('__mocks__/');
+          },
           output: {
             assetFileNames: 'mirador-image-tools.[ext]',
             globals: {
